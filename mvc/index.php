615d68e141b4758593cb7943b055a12e445ba8d0
reaktywa <reaktywa2@gmail.com>
2017-09-21 20:03:48 +0200

    Poprawna paginacja

--- mvc/index.php
+++ mvc/index.php
@@ -1,14 +1,21 @@
 <?php include ('header.php');
 
 	//print_r($_GET);
-	$controllerClassName = $_GET['c'];
-	$functionName = $_GET['f'];
+	//$controllerClassName = $_GET['c'];
+	//$functionName = $_GET['f'];
+
+	$controllerClassName = "NewsController";
+	$functionName = "test";
+
+
 	//$parameters = array_slice($_GET, 2, 2);
 	//echo "controllerClassName=$controllerClassName functionName=$functionName";
 
 	include ".\\controllers\\" . $controllerClassName . ".php";
 	$controller = new $controllerClassName();
     $controller->$functionName();
+
+
 
 	$tab = $_GET = array();
 include ('stopka.php');
