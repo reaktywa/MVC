69054b7e86864b8ccf64272deb534ff96c769b59
reaktywa <reaktywa2@gmail.com>
2017-09-21 21:18:22 +0200

    nowe modele

--- mvc/index.php
+++ mvc/index.php
@@ -3,9 +3,10 @@
 	//print_r($_GET);
 	//$controllerClassName = $_GET['c'];
 	//$functionName = $_GET['f'];
-
-	$controllerClassName = "NewsController";
-	$functionName = "test";
+$c="NewsController";
+$f="test";
+	$controllerClassName = $c;
+	$functionName = $f;
 
 
 	//$parameters = array_slice($_GET, 2, 2);
@@ -14,8 +15,6 @@
 	include ".\\controllers\\" . $controllerClassName . ".php";
 	$controller = new $controllerClassName();
     $controller->$functionName();
-
-
 
 	$tab = $_GET = array();
 include ('stopka.php');
--- mvc/models/NewsModel.php
+++ mvc/models/NewsModel.php
@@ -11,6 +11,11 @@ class NewsModel extends Model{
 
 	public function allRecords(){
 
+		if ($_GET["DO"]=0)
+		{
+			$_GET["DO"]=NULL;
+		}
+
 		$result = $this->mysqli->query("SELECT * FROM news LIMIT 4 OFFSET ".($_GET["DO"]).";");
 		//print_r($result);
 		//$result = $this->mysqli->query("SELECT * FROM news");
