9207b431d13cfb2a6b889c3a70e0958012bcac68
reaktywa <reaktywa2@gmail.com>
2017-10-11 19:55:46 +0200

    banowanie i zabezpieczenia

---
+++ mvc/views/dodajartView.php
@@ -0,0 +1,7 @@
+  <form action='index.php?c=NewsController&f=artk' method='post'>
+    <textarea placeholder='Tytul' name='nTytul'></textarea>
+    <textarea placeholder='Tresc' name='nText'></textarea>
+    <input type='hidden' name='nAuthor' value='2'>
+    <button type='submit' class='btn btn-success green' style='float:right;'><i class='fa fa-share'></i> Wyslij</button>
+    <br>
+    </form>
---
+++ mvc/views/listaView.php
@@ -0,0 +1,27 @@
+<?php
+
+$wynik=count($data);
+while($wynik>0)
+{
+$wynik --;
+echo "<div class='row'><strong><h4>";
+print_r($data[$wynik][0]);
+echo "</h4></strong>  <div>";
+print_r($data[$wynik][1]);
+echo "</div></div><br>";
+echo "<div class='row'>
+<form action='index.php?c=Controller&f=ban' method='post'>
+<input type='hidden' name='userID' value='".($data[$wynik][2])."'>
+<button type='submit'>BAN</button>
+</form>
+
+<form action='index.php?c=Controller&f=unban' method='post'>
+<input type='hidden' name='userID' value='".($data[$wynik][2])."'>
+<button type='submit'>UNBAN</button>
+</form>
+
+</div>
+<hr>
+ ";
+}
+?>
